0c0a1c08f000fe0fc86e831e1ed06b866b738aca
prajwal <kondubhatlav.eprajwal@gmail.com>
2020-04-04 04:47:06 -0700

    add check to commander with options

--- bin/crypto.js
+++ bin/crypto.js
@@ -5,4 +5,5 @@ const package = require("../package.json");
 program
   .version(package.version)
   .command("key", "Manage Key -- Get Key from http://nomics.com")
+  .command("check", "Check price info from http://nomics.com")
   .parse(process.argv);
